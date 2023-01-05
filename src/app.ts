99ead8a1905bf827092533d85a52c938fbb0dfdc
MesutEmpire <samwainaina01@gmail.com>
2023-01-05 14:17:02 +0300

    Intergrate with OpenAI

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
-const { WhatsAppAPI, Handlers, Types } = require("whatsapp-api-js");
-require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
 import { Request, Response, NextFunction, Application } from "express";
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios").default
+const body_parser = require("body-parser");
+const axios = require("axios").default;
+require("dotenv").config();
 
 //express app
 const app: Application = express();
 
-// const whatsapp = new WhatsAppAPI(process.env.TOKEN);
+const configuration = new Configuration({
+  organization: process.env.ORGANIZATION,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 //enable all CORS requests
 app.use(cors({ origin: true, credentials: true }));
@@ -22,32 +28,22 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 
 // routes
 app.get("/webhook/whatsapp", (req: Request, res: Response) => {
-  /**
-   * UPDATE YOUR VERIFY TOKEN
-   *This will be the Verify Token value when you set up webhook
-  **/
-   const verify_token = process.env.VERIFY_TOKEN;
+  const verify_token = process.env.VERIFY_TOKEN;
 
-   // Parse params from the webhook verification request
-   let mode = req.query["hub.mode"];
-   let token = req.query["hub.verify_token"];
-   let challenge = req.query["hub.challenge"];
- 
-   // Check if a token and mode were sent
-   if (mode && token) {
-     // Check the mode and token sent are correct
-     if (mode === "subscribe" && token === verify_token) {
-       // Respond with 200 OK and challenge token from the request
-       console.log("WEBHOOK_VERIFIED");
-       res.status(200).send(challenge);
-     } else {
-       // Responds with '403 Forbidden' if verify tokens do not match
-       res.sendStatus(403);
-     }
-   }
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  if (mode && token) {
+    if (mode === "subscribe" && token === verify_token) {
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
 });
 app.post("/webhook/whatsapp", (req: Request, res: Response) => {
-  const data = req.body;
   if (req.body.object) {
     if (
       req.body.entry &&
@@ -56,33 +52,47 @@ app.post("/webhook/whatsapp", (req: Request, res: Response) => {
       req.body.entry[0].changes[0].value.messages &&
       req.body.entry[0].changes[0].value.messages[0]
     ) {
-      let phone_number_id =req.body.entry[0].changes[0].value.metadata.phone_number_id;
-      let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-      axios({
-        method: "POST", // Required, HTTP method, a string, e.g. POST, GET
-        url:
-          "https://graph.facebook.com/v12.0/" +
-          phone_number_id +
-          "/messages?access_token=" +
-          process.env.WHATSAPP_TOKEN,
-        data: {
-          messaging_product: "whatsapp",
-          to: from,
-          text: { body: "Ack: " + msg_body },
-        },
-        headers: { "Content-Type": "application/json" },
-      });
+      let phone_number_id =
+        req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = req.body.entry[0].changes[0].value.messages[0].from;
+      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
+
+      openai
+        .createCompletion({
+          model: "text-davinci-001",
+          prompt: msg_body,
+          max_tokens: 100,
+          temperature: 0,
+        })
+        .then((response: any) => {
+          console.log(`${response.data.choices[0].text}`);
+          axios
+            .post(
+              `https://graph.facebook.com/v15.0/${phone_number_id}/messages?access_token=${process.env.WHATSAPP_TOKEN}`,
+              {
+                messaging_product: "whatsapp",
+                to: from,
+                text: { body: "Ack: " + msg_body },
+              }
+            )
+            .then((response: unknown) => {
+              console.log(response);
+              res.sendStatus(200);
+            })
+            .catch((error: unknown) => {
+              console.error(error);
+            });
+        })
+        .catch((err: any) => {
+          console.log("ERROR Openai");
+          console.log(err.message);
+        });
     }
-    res.sendStatus(200);
   } else {
-    // Return a '404 Not Found' if event is not from a WhatsApp API
     res.sendStatus(404);
   }
 });
 
-//listening
 app.listen(process.env.PORT, () =>
   console.log("listening on port", process.env.PORT)
 );
-
